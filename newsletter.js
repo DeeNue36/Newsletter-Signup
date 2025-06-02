bc4192b5ba2a02315ca22165a5e799e2e34896a8
Dugiii <dugidemeg@gmail.com>
2025-06-01 20:48:58 -0500

    testing email value and type checks

--- newsletter.js
+++ newsletter.js
@@ -2,4 +2,18 @@ const emailField = document.getElementById('email');
 const submitEmail = document.getElementById('submit-email');
 
 
+submitEmail.addEventListener('click', (e) => {
+    e.preventDefault();
 
+    const emailValue = emailField.value;
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    // if (!emailRegex.test(emailValue)) {
+    //     alert("Please enter a valid email address.");
+    //     return;
+    // }
+    if (emailValue === '') {
+        emailField.classList.toggle('error-mode');
+    }
+    console.log(`Email: ${emailValue}`);
+});
